421d6f17aae8f97edad7a1c91aeb70f83f7041fc
Lu0811 <86023137+Lu0811@users.noreply.github.com>
2024-06-10 07:25:18 -0500

    Update handy.py

--- handy.py
+++ handy.py
@@ -25,20 +25,20 @@ if __name__ == "__main__":
     cv2.createTrackbar('VMin', 'HSV Adjustments', 0, 255, nothing)
     cv2.createTrackbar('VMax', 'HSV Adjustments', 0, 255, nothing)
 
-    # Valores iniciales de HSV
+    # Valores iniciales de HSV (según la imagen)
     cv2.setTrackbarPos('HMin', 'HSV Adjustments', 0)
-    cv2.setTrackbarPos('HMax', 'HSV Adjustments', 178)
-    cv2.setTrackbarPos('SMin', 'HSV Adjustments', 0)
+    cv2.setTrackbarPos('HMax', 'HSV Adjustments', 179)
+    cv2.setTrackbarPos('SMin', 'HSV Adjustments', 41)
     cv2.setTrackbarPos('SMax', 'HSV Adjustments', 255)
-    cv2.setTrackbarPos('VMin', 'HSV Adjustments', 0)
-    cv2.setTrackbarPos('VMax', 'HSV Adjustments', 103)
+    cv2.setTrackbarPos('VMin', 'HSV Adjustments', 44)
+    cv2.setTrackbarPos('VMax', 'HSV Adjustments', 204)
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Obtener valores actuales de las trackbars
+        # valores actuales de las trackbars
         h_min = cv2.getTrackbarPos('HMin', 'HSV Adjustments')
         h_max = cv2.getTrackbarPos('HMax', 'HSV Adjustments')
         s_min = cv2.getTrackbarPos('SMin', 'HSV Adjustments')
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         v_min = cv2.getTrackbarPos('VMin', 'HSV Adjustments')
         v_max = cv2.getTrackbarPos('VMax', 'HSV Adjustments')
 
-        # Actualizar los valores de HSV en tiempo real
+        # los valores de HSV en tiempo real
         detector.hue_lower, detector.hue_upper = h_min, h_max
         detector.sat_lower, detector.sat_upper = s_min, s_max
         detector.val_lower, detector.val_upper = v_min, v_max
